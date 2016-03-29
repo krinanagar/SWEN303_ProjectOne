d9dbf2156414da5434e01451ad2a74853ecd0352
krinanagar <krinanagar@192.168.20.100>
2016-03-29 23:45:51 +1300

    search queries for AND and OR working

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,6 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Colenso Project' });
 });
 
-
 /*Get Browse Page*/
 router.get("/browse",function(req,res){
     client.execute("XQUERY db:list('Colenso')",
@@ -24,6 +23,49 @@ router.get("/browse",function(req,res){
             else {
                 var splitlist = result.result.split("\n")
                 res.render('browse', { title: 'Colenso Project', results: splitlist });
+            }
+        }
+    );
+});
+
+/*Get Search Page*/
+router.get('/search', function(req, res) {
+
+    var tei = "XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';";
+    if(req.query.searchString){
+        var searchStrArray = req.query.searchString.split(" ");
+        var queryType = "";
+
+        var i = 0;
+        while(i < searchStrArray.length){
+            queryType += searchStrArray[i];
+            ++i;
+            if(i < searchStrArray.length){
+                if(searchStrArray[i] ==="OR"){
+                    queryType += "' ftor '";
+                }else if(searchStrArray[i] === "AND"){
+                    queryType += "' ftand '";
+                }else if(searchStrArray[i] === "NOT"){
+                    queryType += "' ftnot '";
+                }
+            }
+            ++i;
+        }
+    }
+    var query = tei + "for $t in (collection('Colenso')[. contains text ' "  + queryType +"'])\n" +
+        "return concat('<a href=\"/file?filename=', db:path($t), '\" class=\"searchResult\">', '</a>'," +
+        "'<p class=\"searchResult\">', db:path($t), '</p>')";
+    client.execute(query,
+        function (error, result) {
+            if(error){ console.error(error)}
+            else {
+                if(req.query.searchString == undefined || req.query.searchString == null){
+                    res.render('search', { title: 'Colenso Databse', results: " "});
+                }else{
+                    var nResults = (result.result.match(/<\/a>/g) || []).length;
+                    var splitlist = result.result.split("\n")
+                    res.render('search', { title: 'Colenso Project', results: splitlist , nResults : nResults});
+                }
             }
         }
     );
@@ -61,4 +103,5 @@ router.get("/XQuery",function(req,res){
         }
     );
 });
+
 module.exports = router;
